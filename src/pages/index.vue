efd6c2140eaee1d3bfdebde402f8978624f72e17
yauyy <work_e_mail@126.com>
2024-05-17 21:50:18 +0800

    🎉 init: init

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,139 @@
-<script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
-})
+<script setup lang="ts">
+interface BlockState {
+  x: number
+  y: number
+  adjacentMines: number
+  revealed: boolean
+  mine?: boolean
+  flagged?: boolean
+}
 
-const name = ref('')
+const WIDTH = 10
+const HEIGHT = 10
 
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+const state = reactive(Array.from({ length: HEIGHT }, (_, y) => Array.from({ length: WIDTH }, (_, x): BlockState => ({
+  x,
+  y,
+  revealed: false,
+  adjacentMines: 0,
+}))))
+// 生成雷
+function generateMines(initial: BlockState) {
+  for (const row of state) {
+    for (const block of row) {
+      if (Math.abs(initial.x - block.x) <= 1)
+        continue
+      if (Math.abs(initial.y - block.y) <= 1)
+        continue
+
+      block.mine = Math.random() < 0.3
+    }
+  }
+
+  updateNumbers()
+}
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, 1],
+  [0, -1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+]
+
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-orange-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-pink-500',
+  'text-gray-500',
+]
+// 计算邻居雷数
+function updateNumbers() {
+  state.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine)
+        return
+
+      getSliblings(block).forEach((b) => {
+        if (b.mine)
+          block.adjacentMines += 1
+      })
+    })
+  })
+}
+
+// 获取邻居
+function getSliblings(block: BlockState) {
+  return directions.map(([dx, dy]) => {
+    const y2 = block.y + dy
+    const x2 = block.x + dx
+    if (y2 < 0 || y2 >= HEIGHT || x2 < 0 || x2 >= WIDTH)
+      return void 0
+
+    return state[y2][x2]
+  }).filter(Boolean) as BlockState[]
+}
+
+function getBlockClass(block: BlockState) {
+  if (!block.revealed)
+    return 'bg-gray-500/10'
+
+  return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
+}
+
+// 过滤出0
+function expendZero(block: BlockState) {
+  if (block.adjacentMines)
+    return
+
+  getSliblings(block).forEach((s) => {
+    if (!s.revealed) {
+      s.revealed = true
+      expendZero(s)
+    }
+  })
+}
+
+let mineGenerated = false
+const isDev = true
+
+function onClick(block: BlockState) {
+  if (!mineGenerated) {
+    generateMines(block)
+    mineGenerated = true
+  }
+  block.revealed = true
+  if (block.mine) {
+    // Game over
+    alert('Game over')
+  }
+
+  expendZero(block)
 }
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
+    <h1>Welcome to Vue Minsweeper</h1>
     <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+      <div v-for="(row, y) in state" :key="y" flex="~" items-center justify-center>
+        <button v-for="(block, x) in row" :key="x" :class="getBlockClass(block)" flex="~" m-0.5 h-10 w-10 items-center justify-center border vertical-top hover="bg-gray/50" @click="onClick(block)">
+          <template v-if="block.revealed || isDev">
+            <div v-if="block.mine" i-mdi:mine />
+            <div v-else>
+              {{ block.adjacentMines || '0' }}
+            </div>
+          </template>
+        </button>
+      </div>
     </div>
   </div>
 </template>
